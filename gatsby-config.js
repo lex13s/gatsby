37ad40739165c6412fc5555663282ad9900db265
Alexey S <jobmy@mail.ru>
2020-10-24 13:29:48 +0300

    init

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,20 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+if (process.env.NODE_ENV === "development") {
+  require("dotenv").config();
+}
+
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    {
+      resolve: `gatsby-source-contentful`,
+      options: {
+        spaceId: `804s3kognsxd`,
+        // Learn about environment variables: https://gatsby.dev/env-vars
+        accessToken: `dB1ldUzEM1wFDzBwNht9vd3Gi7v-LReK794e46BAhs0`,
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,20 @@
 import React from "react"
+import { graphql, useStaticQuery } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  const { contentfulTitle: { title, titleSecond } } = useStaticQuery(graphql`
+    query MyQuery {
+      contentfulTitle {
+        title
+        titleSecond
+      }
+     }
+  `)
+
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{titleSecond}</p>
+    </>
+  )
 }
